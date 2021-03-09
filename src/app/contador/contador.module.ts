99ceda463e0d4972fc1962a2107337a18cef56da
PedroAvila <minimainor_austin@hotmail.com>
2021-03-09 08:58:36 -0500

    Primer commit

---
+++ src/app/contador/contador.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from "@angular/core";
+import { ContadorComponent } from './contador/contador.components';
+
+@NgModule({
+  declarations: [
+      ContadorComponent,
+  ],
+  exports: [
+      ContadorComponent,
+  ]
+})
+
+export class ContadorModule {}
